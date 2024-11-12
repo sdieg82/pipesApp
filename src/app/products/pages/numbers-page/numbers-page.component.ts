0e3b66f8ce80b284d1286cc95bc5f278a4ae6671
Diego <sdieg82@gmail.com>
2024-11-12 09:41:36 -0500

    numbers pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PanelModule } from 'primeng/panel';
+import {CardModule} from 'primeng/card'
+
 
 @Component({
   selector: 'app-numbers-page',
   standalone: true,
-  imports: [],
+  imports: [CardModule,PanelModule,CommonModule],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
+
+  public totalSells:number=215622.5566
+  public percent:number=0.5566
 
 }
